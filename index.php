73ec1ee4b7841d7f42642eab2a97dac873f49fda
Idan Hazan <as4ny0o@gmail.com>
2019-11-16 00:58:16 +0200

    PWA basic support

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta name="keywords" content="IT,Shifts,TowerJazz">
         <meta name="author" content="Idan Hazan">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <link rel="manifest" href="manifest.json">
         <title>IT-Shifts</title>
     </head>
     <body>
